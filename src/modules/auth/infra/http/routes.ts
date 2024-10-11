f8afbb8f998c7b75b8d5721806f95efc81b92523
TiagoM13 <thyagomotha2000@gmail.com>
2024-10-11 11:22:20 -0300

    feat: remove middleware

--- src/modules/auth/infra/http/routes.ts
+++ src/modules/auth/infra/http/routes.ts
@@ -1,16 +1,12 @@
 import { FastifyInstance } from 'fastify';
 
 import { bindController } from '@app/infra/http/controller/bindController';
-import { updateLastAccess } from '@app/infra/http/middleware';
 import {
   makeForgotPasswordController,
   makeLoginController,
 } from '@shared/factories/controllers';
-import { makeUserRepository } from '@shared/factories/repositories/make-user-repository';
 
 const authRoutes = async (app: FastifyInstance) => {
-  app.addHook('preHandler', updateLastAccess(makeUserRepository()));
-
   app.post('/auth/sign-in', bindController(makeLoginController()));
   app.post(
     '/auth/forgot-password',
