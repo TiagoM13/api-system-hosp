c2ef1b0e5f0a3657490198f58b60ce2e8638e43e
TiagoM13 <thyagomotha2000@gmail.com>
2024-11-05 10:00:40 -0300

    feat: add rules in schemas

--- src/modules/appointments/schemas/index.ts
+++ src/modules/appointments/schemas/index.ts
@@ -1,5 +1,7 @@
 import z from 'zod';
 
+import { AppError } from '@app/errors/app-client';
+
 export const appointmentParamId = z.object({
   patientId: z.string().uuid(),
 });
@@ -16,12 +18,37 @@ export const appointmentQuerySchema = z
     items_per_page: z.coerce.number().max(500).default(10),
     appointment_type: z.string().optional(),
     start_date: z.preprocess(
-      val => (typeof val === 'string' ? new Date(val) : val),
-      z.date().optional(),
+      val => {
+        if (typeof val === 'string' && val.trim() === '') return undefined;
+
+        if (typeof val === 'string' && !/^\d{4}-\d{2}-\d{2}$/.test(val)) {
+          throw new AppError(
+            'Data de início inválida. Use o formato YYYY-MM-DD',
+          );
+        }
+        return typeof val === 'string' ? new Date(val) : val;
+      },
+      z
+        .date({
+          invalid_type_error:
+            'Data de início inválida. Use o formato YYYY-MM-DD',
+        })
+        .optional(),
     ),
     end_date: z.preprocess(
-      val => (typeof val === 'string' ? new Date(val) : val),
-      z.date().optional(),
+      val => {
+        if (typeof val === 'string' && val.trim() === '') return undefined;
+
+        if (typeof val === 'string' && !/^\d{4}-\d{2}-\d{2}$/.test(val)) {
+          throw new AppError('Data de fim inválida. Use o formato YYYY-MM-DD');
+        }
+        return typeof val === 'string' ? new Date(val) : val;
+      },
+      z
+        .date({
+          invalid_type_error: 'Data de fim inválida. Use o formato YYYY-MM-DD',
+        })
+        .optional(),
     ),
   })
   .refine(
@@ -34,17 +61,5 @@ export const appointmentQuerySchema = z
     {
       message: 'A data de início não pode ser maior que a data de fim',
       path: ['start_date'],
-    },
-  )
-  .refine(
-    data => {
-      if (data.start_date && data.end_date) {
-        return data.end_date >= data.start_date;
-      }
-      return true;
-    },
-    {
-      message: 'A data de fim não pode ser menor que a data de início',
-      path: ['end_date'],
     },
   );
