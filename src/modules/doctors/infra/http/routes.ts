3d1ef83747682f80495b819ce45e91fced146861
TiagoM13 <thyagomotha2000@gmail.com>
2024-12-20 10:29:48 -0300

    feat: add configs and add permissions

--- src/modules/doctors/infra/http/routes.ts
+++ src/modules/doctors/infra/http/routes.ts
@@ -17,7 +17,10 @@ import {
 import { makeUserRepository } from '@shared/factories/repositories';
 
 export const doctorRoutes = async (app: FastifyInstance) => {
-  app.addHook('preHandler', verifyAuthorization([Role.ADMIN]));
+  app.addHook(
+    'preHandler',
+    verifyAuthorization([Role.ADMIN, Role.EDITOR, Role.CLINICAL]),
+  );
   app.addHook('preHandler', updateLastAccess(makeUserRepository()));
 
   app.get('/doctors', bindController(makeGetAllDoctorsController()));
--- src/shared/entities/index.ts
+++ src/shared/entities/index.ts
@@ -2,7 +2,6 @@ export * from './patient';
 export * from './appointment';
 export * from './user';
 export * from './metadata';
-export * from './params';
 export * from './paginate';
 export * from './entities';
 export * from './doctor';
